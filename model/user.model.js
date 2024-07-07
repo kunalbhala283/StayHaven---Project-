9e7a6dc724be37a71cfc31ed7e444202ec18dadb
Kunal Bhala <bhalakunal1210@gmail.com>
2024-07-07 15:53:32 +0530

    add timestamps of user

--- model/user.model.js
+++ model/user.model.js
@@ -7,6 +7,8 @@ const userSchema = new mongoose.Schema({
     password: {type:String,required:true},
     email: {type:String,required:true,unique: true},
 
+},{
+    timestamps:true,
 })
 
 const User = mongoose.model("User",userSchema)
