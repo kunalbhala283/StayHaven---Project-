c210b21c46fbd78319c9f03de37c4464b6b23164
Kunal Bhala <bhalakunal1210@gmail.com>
2024-07-10 16:06:53 +0530

    UPDATES

--- server.js
+++ server.js
@@ -19,21 +19,22 @@ const connectDB = require('./config/dbconfig')
 const app = express();
 
 app.use(cors());
- app.use(express.json())
- connectDB();
- const PORT = 3500;
- 
+
+app.use(express.json())
+connectDB();
+const PORT = 3500;
+
 app.get('/', (req, res) => {
     res.send("Hello World ")
 })
 
 app.use("/api/hotels", hotelRouter)
-app.use("/api/categories",categoryRouter)
-app.use("/api/categorydata",categorydataAddedtoDBRouteer)
-app.use('/api/hoteldata',hoteldataAddedtoDBRouteer)
-app.use('/api/hotel',singlehotelrouterbyID)
-app.use('/api/auth',authrouter)
-app.use('/api/wishlist',wishlistRouter)
+app.use("/api/categories", categoryRouter)
+app.use("/api/categorydata", categorydataAddedtoDBRouteer)
+app.use('/api/hoteldata', hoteldataAddedtoDBRouteer)
+app.use('/api/hotel', singlehotelrouterbyID)
+app.use('/api/auth', authrouter)
+app.use('/api/wishlist', wishlistRouter)
 
 
 mongoose.connection.once('open', () => {
